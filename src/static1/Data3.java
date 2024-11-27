d7f0d71801ad0008d8adff0e9cedde75a8f35a66
ynyejn <yeounshi@gmail.com>
2024-11-28 02:03:54 +0900

    [JB-8] static변수3

--- src/static1/Data3.java
+++ src/static1/Data3.java
@@ -1,10 +1,9 @@
 package static1;
 
 public class Data3 {
-    public String name;
-    public static int count;
-    // static변수, 정적변수,클래스 변수 라고 불림.
-    // 메소드 영역에서 관리함!
+    public String name;         // 멤버변수->인스턴스 변수
+    public static int count;    // 멤버변수->클래스 변수, 정적변수, static변수
+    // 메소드 영역에서 관리함! 자바프로그램 시작할 때 딱 1개 만들어짐.
 
 
     public Data3(String name) {
--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -11,5 +11,16 @@ public class DataCountMain3 {
 
         Data3 c = new Data3("C");
         System.out.println("C count: " + Data3.count);
+
+        //추가
+        //인스턴스를 통한 접근
+        Data3 d = new Data3("D");
+        System.out.println(d.count);
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
+
+        //둘다 가능함. 하지만 정적변수는 클래스를 통한 접근을 권장함
+        //코드를 읽을때 마치 인스턴스 변수를 읽는것처럼 보이기 때문
     }
 }
