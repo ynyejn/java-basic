37db0c51fd1030994dc77e93974b31f37f40d5c0
ynyejn <yeounshi@gmail.com>
2024-11-28 01:26:45 +0900

    [JB-8] 스택영역과 힙영역

---
+++ src/memory/Data.java
@@ -0,0 +1,12 @@
+package memory;
+
+public class Data {
+    private int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
