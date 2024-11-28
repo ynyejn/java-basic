d305919d3d13cb96fc165d9ef06c0b31c6269593
ynyejn <yeounshi@gmail.com>
2024-11-29 00:53:58 +0900

    [JB-8] static 메서드1 static이 붙은 정적 메서드는 객체생성없이 클래스명으로 바로 호출할 수 있다.

---
+++ src/static2/DecoMain2.java
@@ -0,0 +1,11 @@
+package static2;
+
+public class DecoMain2 {
+    public static void main(String[] args) {
+        String s = "Hello Java";
+        String result = DecoUtil2.deco(s);
+
+        System.out.println("before: " + s);
+        System.out.println("after: " + result);
+    }
+}
